ba511f0811a625f2accd01adf83077f903bc37d8
arifeensifat <arifeen.sifat@gmail.com>
2023-03-11 22:25:00 +0600

    portfolio

---
+++ script.js
@@ -0,0 +1,11 @@
+var typed = new Typed(".typed", {
+    strings: ["Forntend Developer" ,"Developer" ],
+    typeSpeed: 60,
+    startDelay: 900,
+    showCursor: false,
+    loop: true,
+    backDelay: 900,
+    backSpeed: 40,
+  });
+
+  let
